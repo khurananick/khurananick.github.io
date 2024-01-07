3b1da1ae72b5e1766232e3398a016397cdfe713a
khurananick <khurananick@gmail.com>
2024-01-07 10:06:51 -0500

    Updates

--- README.md
+++
@@ -1,4 +0,0 @@
-# Pulak (Nick) Khurana
-## This is my personal website built on React.
-
-Check it out here: [https://khurananick.github.io](https://khurananick.github.io)
--- src/__tests__/Helpers/shuffleArray.test.js
+++
@@ -1,7 +0,0 @@
-import shuffleArray from "../../Helpers/shuffleArray";
-
-test("shuffles array successfully", () => {
-  const array = [1, 2, 3, 4, 5];
-  const shuffledArray = shuffleArray([...array]);
-  expect(shuffledArray).not.toEqual(array);
-});
